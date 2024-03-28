07acdff26fe33d88dfd5cedc5cb7cfc4b6ad4041
Vinny <vinny@Vinnys-MBP.fios-router.home>
2024-03-28 10:41:23 -0400

    Initial button styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="buttons flex"> 
+        <div className="btn btn-red">+100</div>
+        <div className="btn btn-red">+10</div>
+        <div className="btn btn-red">+1</div>
+        <div className="btn btn-blue">+1</div>
+        <div className="btn btn-blue">+10</div>
+        <div className="btn btn-blue">+100</div>
+      </div>
+      
     </div>
   );
 }
