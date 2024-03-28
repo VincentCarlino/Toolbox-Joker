55ff91004b44beb228ace08ac25521ab780fa099
Vincent <vjcarlino22@gmail.com>
2024-03-28 18:48:15 -0400

    Display score

--- src/App.js
+++ src/App.js
@@ -6,13 +6,24 @@ function App() {
     <div className="app">
       <div className="crt crt-flicker"></div>
       <div className="page-container crt-bloom">
+        <div className="flex scores">
+        <div className="score score-chips">
+          0
+        </div>
+        <div className="times">
+          X
+        </div>
+        <div className="score score-mult">
+          0
+        </div>
+        </div>
         <div className="buttons flex"> 
-          <div className="btn btn-red">+100</div>
-          <div className="btn btn-red">+10</div>
-          <div className="btn btn-red">+1</div>
           <div className="btn btn-blue">+1</div>
           <div className="btn btn-blue">+10</div>
           <div className="btn btn-blue">+100</div>
+          <div className="btn btn-red">+100</div>
+          <div className="btn btn-red">+10</div>
+          <div className="btn btn-red">+1</div>
         </div>
         <h1>Jokers</h1>
         <img src={jokers} className="jokers"/>
